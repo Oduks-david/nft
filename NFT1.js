fdbd139eb24b310f792c3a73d4cdf35907a06894
Oduks-david <103524307+Oduks-david@users.noreply.github.com>
2022-08-11 04:40:54 -0700

    Add files via upload

---
+++ NFT1.js
@@ -0,0 +1,23 @@
+function change() {
+    var x = document.getElementById("red")
+
+    if (x.style.color === "white"){
+        x.style.color = "red";
+    } else{
+        x.style.color = "white"
+    }
+}
+
+
+// for the icon lover
+
+function change() {
+    var x = document.getElementById("lover")
+
+    if (x.style.color === "white"){
+        x.style.color = "red";
+    } else{
+        x.style.color = "white"
+    }
+}
+
